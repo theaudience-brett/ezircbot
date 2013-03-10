3321d11dbae3f15d15e60aa0cfd6d85527684f6c
Brett Mack <mack.brett@gmail.com>
2013-03-10 08:02:50 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ircbotadmin::Application.config.secret_token = 'de4a55d1d0c85ab039ce4c2537fd2884c0c67c62dc987d991375d6e28eb981408b397a6f866569ee15426346ca3eca431cf7c9e198fbfcbe6feab29db15b2f8a'
